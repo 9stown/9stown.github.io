1eccaaf913382918febc7510baf99e1f33b55601
Refresh4Ever <149150346+refresh4ever@users.noreply.github.com>
2024-09-22 10:43:23 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,47 +1,39 @@
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
-
-<script>
-  // Configuration Firebase (à récupérer dans la console Firebase)
-  const firebaseConfig = {
-    apiKey: "AIzaSyB6TIkXJJjCO5lubIAmla2HmIaRwuFq5qE",
-    authDomain: "refresh4ever-dd97b.firebaseapp.com",
-    projectId: "refresh4ever-dd97b",
-    storageBucket: "refresh4ever-dd97b.appspot.com",
-    messagingSenderId: "391655254933",
-    appId: "1:391655254933:web:6f70a2ac65d264c69e649c"
-  };
-
-// Initialiser Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
-// Fonction pour s'inscrire
+// Register
 function register(event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  auth.createUserWithEmailAndPassword(email, password)
+  firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      console.log('Inscription réussie:', userCredential.user);
+      // Correct
+      const user = userCredential.user;
+      alert('Inscription réussie, utilisateur: ' + user.email);
     })
     .catch((error) => {
-      console.error('Erreur lors de l\'inscription:', error.message);
+      // Error
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert('Erreur lors de l\'inscription: ' + errorMessage);
     });
 }
 
-// Fonction pour se connecter
+// Login
 function login(event) {
   event.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  auth.signInWithEmailAndPassword(email, password)
+  firebase.auth().signInWithEmailAndPassword(email, password)
     .then((userCredential) => {
-      console.log('Connexion réussie:', userCredential.user);
+      // Correct
+      const user = userCredential.user;
+      alert('Connexion réussie, utilisateur: ' + user.email);
     })
     .catch((error) => {
-      console.error('Erreur lors de la connexion:', error.message);
+      // Error
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert('Erreur lors de la connexion: ' + errorMessage);
     });
 }
