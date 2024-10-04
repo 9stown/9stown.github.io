d051fdac46710610a8f922290399214f6ca31130
Refresh4Ever <149150346+refresh4ever@users.noreply.github.com>
2024-10-04 12:53:30 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,25 +1,27 @@
-// Système de mode sombre/clair
 const themeToggle = document.querySelector('.theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const body = document.body;
 
-// Initialisation du mode en fonction du localStorage
-if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-mode');
-    themeIcon.setAttribute("fill", "#ffffff"); // Couleur claire pour l'icône en mode sombre
-} else {
-    themeIcon.setAttribute("fill", "#111111"); // Couleur sombre pour l'icône en mode clair
+let isDarkMode = false;
+
+function setTheme() {
+    if (isDarkMode) {
+        body.classList.add('dark-theme');
+        body.classList.remove('light-theme');
+        themeIcon.setAttribute('fill', '#f0f0f0');  // Icône en blanc pour le thème sombre
+    } else {
+        body.classList.add('light-theme');
+        body.classList.remove('dark-theme');
+        themeIcon.setAttribute('fill', '#111111');  // Icône en noir pour le thème clair
+    }
 }
 
 themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    
-    if (body.classList.contains('dark-mode')) {
-        localStorage.setItem('theme', 'dark');
-        themeIcon.setAttribute("fill", "#ffffff");
-    } else {
-        localStorage.setItem('theme', 'light');
-        themeIcon.setAttribute("fill", "#111111");
-    }
+    isDarkMode = !isDarkMode;
+    setTheme();
 });
+
+// Initialiser le thème sur le mode clair
+isDarkMode = false;
+setTheme();
 
