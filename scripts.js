7a1b1f3b41f59024e05105fc849ffc2d4dea4cdd
Refresh4Ever <149150346+refresh4ever@users.noreply.github.com>
2024-10-05 10:38:44 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -29,10 +29,15 @@ window.addEventListener('scroll', function() {
     document.querySelector('header').style.backgroundPositionY = `${scrollPos * 0.5}px`;
 });
 
+// Barre de progression du scroll
+window.addEventListener('scroll', function() {
+    const scrolled = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
+    document.querySelector('.scroll-progress').style.width = `${scrolled * 100}%`;
+});
+
 // Confirmation après l'envoi du formulaire
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Votre message a été envoyé avec succès !');
     this.reset();  // Réinitialise le formulaire
 });
-
